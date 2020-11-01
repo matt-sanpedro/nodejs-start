1e807daee555172226c0388df4871ba7121733a4
matt-sanpedro <mattsanpedro76@gmail.com>
2020-10-31 18:48:58 -0700

    added random supervillains functionality

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 // jshint esversion:6
 
-// this will never change, will throw error
-const fs = require("fs");
+// // this will never change, will throw error
+// const fs = require("fs");
+// fs.copyFileSync("file1.txt", "copy.txt");
 
-fs.copyFileSync("file1.txt", "copy.txt");
+// experiment with random superhero names
+var superheroes = require("superheroes");
+var mySuperHeroName = superheroes.random();
+
+console.log(mySuperHeroName);
+
+// experiment with random villain names
+var supervillains = require("supervillains");
+var mySuperVillainName = supervillains.random();
+
+console.log(mySuperVillainName);
